7be289b30a7dc0473b5a0c1f11bb960d200bc89b
alamin <amin.se.aiub@gmail.com>
2023-10-04 13:35:41 +0600

    into php

---
+++ php/test.php
@@ -0,0 +1,56 @@
+<?php 
+
+/*     $name = "alamin";
+    $id = 123;
+    $cgpa = 3.4;
+
+    $student = [1,'alamin', 3.5];
+    $student = array(1,'alamin', 3.5);
+    $student[0];
+
+    $students = [
+                    [1,'alamin', 3.5],
+                    [1,'alamin', 3.5],
+                    [1,'alamin', 3.5]
+                ];
+    $students[2][2];
+
+    $student = ['id'=>1,'name'=>'alamin', 'cgpa'=>3.5];
+    $student['cgpa'];
+    $students = [
+        's1'=>['id'=>1,'name'=>'alamin', 'cgpa'=>3.5],
+        's2'=>['id'=>1,'name'=>'alamin', 'cgpa'=>3.5],
+        's3'=>['id'=>1,'name'=>'alamin', 'cgpa'=>3.5]
+    ];
+    $students['s3']['cgpa'];
+
+    function sum($a=10, $b=3){
+        return $a+$b;
+    }
+    $v = sum();
+
+    if(){
+
+    }else if(){
+
+    }
+
+    for($i=0; $i<10; $i++){
+
+    } */
+    //print("Hello PHP!".$id);
+    //echo "another print option";
+
+    // $students = [
+    //     's1'=>['id'=>1,'name'=>'alamin', 'cgpa'=>3.5],
+    //     's2'=>['id'=>1,'name'=>'alamin', 'cgpa'=>3.5],
+    //     's3'=>['id'=>1,'name'=>'alamin', 'cgpa'=>3.5]
+    // ];
+    //print_r($students);
+
+    for($i=0; $i<10; $i++){
+        echo "<h3>This is webtech course...</h3>";
+    }
+?>
+
+<h2>Test</h2>
